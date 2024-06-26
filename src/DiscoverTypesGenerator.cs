245814157c8b3ade04975540549c7450c55df4fc
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-06-26 15:59:42 +0200

    Update DiscoverTypesGenerator.cs

--- src/DiscoverTypesGenerator.cs
+++ src/DiscoverTypesGenerator.cs
@@ -19,26 +19,25 @@ public sealed class DiscoverTypesGenerator : ISourceGenerator
         HashSet<string> assemblyLocations = new HashSet<string>();
 
         // Get all the referenced assemblies
-        Compilation compilation = context.Compilation;
-        foreach (AssemblyIdentity? reference in compilation.ReferencedAssemblyNames)
+        foreach (MetadataReference? reference in context.Compilation.References)
         {
-            IAssemblySymbol? assembly = compilation.GetAssemblyOrModuleSymbol(reference) as IAssemblySymbol;
-            if (assembly != null && !string.IsNullOrEmpty(assembly.Locations.FirstOrDefault()?.SourceTree?.FilePath))
+            PortableExecutableReference? metadataReference = reference as PortableExecutableReference;
+            if (metadataReference != null && !string.IsNullOrEmpty(metadataReference.FilePath))
             {
-                assemblyLocations.Add(assembly.Locations.First().SourceTree.FilePath);
+                assemblyLocations.Add(metadataReference.FilePath);
             }
         }
 
         // Generate source code
         string source = GenerateSource(assemblyLocations);
-        context.AddSource("DiscoveredTypes.g.cs", SourceText.From(source, Encoding.UTF8));
+        context.AddSource("DiscoveredAssemblies.g.cs", SourceText.From(source, Encoding.UTF8));
     }
 
     private string GenerateSource(IEnumerable<string> assemblyLocations)
     {
         StringBuilder sb = new StringBuilder();
         sb.AppendLine("using System.Collections.Generic;");
-        sb.AppendLine("namespace Generated");
+        sb.AppendLine("namespace Nefarius.Utilities.WixSharp");
         sb.AppendLine("{");
         sb.AppendLine("    public static class DiscoveredAssemblies");
         sb.AppendLine("    {");
