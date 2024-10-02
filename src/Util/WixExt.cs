13e3f49d2f5e6fe3d2e56e186a28ecc1e93f7524
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-10-02 19:28:45 +0200

    Added GetSubDirectories

---
+++ src/Util/WixExt.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+using WixSharp;
+
+namespace Nefarius.Utilities.WixSharp.Util;
+
+/// <summary>
+///     WixSharp extensions and utilities.
+/// </summary>
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+[SuppressMessage("ReSharper", "UnusedType.Global")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public static class WixExt
+{
+    /// <summary>
+    ///     Recursively resolves all subdirectories and their containing files.
+    /// </summary>
+    public static List<Dir> GetSubDirectories(Feature feature, string directory)
+    {
+        List<Dir> subDirectoryInfosCollection = new();
+
+        foreach (string subDirectory in Directory.GetDirectories(directory))
+        {
+            string subDirectoryName = subDirectory.Remove(0, subDirectory.LastIndexOf('\\') + 1);
+            Dir newDir =
+                new(feature, subDirectoryName, new Files(feature, subDirectory + @"\*.*")) { Name = subDirectoryName };
+            subDirectoryInfosCollection.Add(newDir);
+
+            // Recursively traverse nested directories
+            GetSubDirectories(feature, subDirectory);
+        }
+
+        return subDirectoryInfosCollection;
+    }
+}
